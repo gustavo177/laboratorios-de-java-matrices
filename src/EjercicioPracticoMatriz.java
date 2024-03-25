b8e60194ef19899276f313c3ada845c348ca70cf
gustavo177 <gtvadf177@hotmail.com>
2024-03-25 16:24:36 -0500

    Capturamos el tamaño de la matriz y controlamos los errores, si la matriz tiene un cero como tamaño mostrara un ERROR como respuesta

--- src/EjercicioPracticoMatriz.java
+++ src/EjercicioPracticoMatriz.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /*
 * Escribir un programa que imprima una X construida a base de la letra X
 * en las diagonales de la matriz y utilizar el carácter guion bajo como relleno.
@@ -25,34 +27,44 @@ Si n es igual a cero imprimir "ERROR" y finalizar la ejecución del programa.
 * */
 public class EjercicioPracticoMatriz {
     public static void main(String[] args) {
-        String[][] matriz = new String[6][6];
-        int c = matriz.length;
+        System.out.println("Ingrese el tamaño de n");
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
 
-        for (int i = 0; i < matriz.length; i++) {
-            for (int j = 0; j < matriz[i].length; j++) {
-                if (i == j) {
-                    matriz[i][j] = "X";
+        if (n == 0) {
+            System.err.println("ERROR");
+        } else {
+            String[][] matriz = new String[6][6];
+            int c = matriz.length;
+
+            for (int i = 0; i < matriz.length; i++) {
+                for (int j = 0; j < matriz[i].length; j++) {
+                    if (i == j) {
+                        matriz[i][j] = "X";
+                    }
+
+                    if (matriz[i][j] == "X") {
+                        continue;
+                    } else {
+                        matriz[i][j] = "_";
+                    }
+
                 }
 
-                if(matriz[i][j] == "X"){
-                    continue;
-                }else{
-                    matriz[i][j]="_";
+                c = c - 1;
+                if (i < matriz.length) {
+                    matriz[i][c] = "X";
                 }
-
             }
 
-            c=c-1;
-            if (i < matriz.length){
-                matriz[i][c] = "X";
+            for (int i = 0; i < matriz.length; i++) {
+                for (int j = 0; j < matriz[i].length; j++) {
+                    System.out.print(matriz[i][j] + "\t");
+                }
+                System.out.println();
             }
         }
 
-        for (int i = 0; i < matriz.length; i++) {
-            for (int j = 0; j < matriz[i].length; j++) {
-                System.out.print(matriz[i][j] + "\t");
-            }
-            System.out.println();
-        }
+
     }
 }
